ee3225f389fcdcd60f00c4782edebd0f739a907e
kelly <kellycruz.ti@gmail.com>
2019-05-05 19:48:24 -0300

    update models

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,4 +1,5 @@
 class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
+  enum status: { waiting: 0, delivered: 1 }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :product_category
   has_many :order_products
+  validates_associated :product_category
+  validates :name, presence: true
+  validates :price, presence: true
 end
