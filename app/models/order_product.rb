03b197c6388d21958515455b16f5c3ffdb1a459d
kelly <kellycruz.ti@gmail.com>
2019-05-06 23:25:12 -0300

    Add Active Storage in model

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,5 +1,8 @@
 class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
-  enum status: { waiting: 0, delivered: 1 }
+
+  validates_associated :order
+  validates_associated :product
+  validates :quantity, presence: true
 end
